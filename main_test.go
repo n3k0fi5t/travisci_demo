8aaed8e7d948a49eb808241e7fab855f8ea9fe83
n3k0fi5t <r05922078@ntu.edu.tw>
2021-04-23 13:33:54 +0800

    remove testify and change the test
    Signed-off-by: n3k0fi5t <r05922078@ntu.edu.tw>

--- main_test.go
+++ main_test.go
@@ -4,10 +4,34 @@ import (
 	"testing"
 
 	main "github.com/n3k0fi5t/travisci_demo"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestWork(t *testing.T) {
-	res := main.SomeWork(5)
-	assert.Equal(t, res, true)
+func TestSomeWork(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "single Pass",
+			args: args{1},
+			want: true,
+		},
+		{
+			name: "multiple Pass",
+			args: args{5},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := main.SomeWork(tt.args.n); got != tt.want {
+				t.Errorf("SomeWork() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
